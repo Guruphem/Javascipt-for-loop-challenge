cd51b49c823b75cbd3f6fe2e48036cbefdd084fe
Guruphem <leoguruphem@gmail.com>
2023-12-22 10:22:59 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/* QUESTION:
+ WRITE A JAVSCRIPT CODE TO PRINT NUMBER BACKWARDS FROM 20 TO 1 IN AN ARRAY USING FOR LOOP
+
+*/
+
+var arrayLog = [];
+
+for (var i = 20; i > 0; i--) {
+  arrayLog.push(i);
+}
+
+console.log(arrayLog);
